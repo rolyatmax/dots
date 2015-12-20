d52a60a0cbadc300702271458313176278f80a34
Taylor Baldwin <taylorbaldwin@gmail.com>
2015-12-20 15:17:55 -0500

    markdownify

--- js/main.js
+++ js/main.js
@@ -1,35 +1,13 @@
 /* globals Info */
 import 'babel-polyfill';
-import {markdown} from 'markdown';
-
-require('./sketch');
-require('./gui');
-
-
-function httpGet(url) {
-    return new Promise(function(resolve, reject) {
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.onload = function() {
-            if (this.status >= 200 && this.status < 400) {
-                resolve(this.response);
-            } else {
-                reject(this.response);
-            }
-        };
-        request.onerror = function() {
-            reject(this.response);
-        };
-        request.send();
-    });
-}
+import readme from '../README.md';
+import './sketch';
+import './gui';
 
 
-httpGet('README.md').then(readme => {
-    new Info({
-        html: markdown.toHTML(readme),
-        el: 'info',
-        container: 'container',
-        keyTrigger: true
-    });
+new Info({
+    html: readme,
+    el: 'info',
+    container: 'container',
+    keyTrigger: true
 });
